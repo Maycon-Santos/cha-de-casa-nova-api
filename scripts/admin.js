770596c4706c09240f9d6432bc07c86ce1f360fa
Maycon Santos <maycon.santos@d3.do>
2019-09-18 20:24:27 -0300

    Add command to list all admins

--- scripts/admin.js
+++ scripts/admin.js
@@ -3,31 +3,48 @@ const { resolve } = require('path')
 const sqlite3 = require('sqlite3').verbose()
 const db = new sqlite3.Database(resolve('database/adonis.sqlite'))
 
-function getUserID () {
-  let paramIndex = process.argv.indexOf('--user-id')
+// Queries
+const makeQuery = (admin, userID) => `UPDATE users SET admin=${admin} WHERE id=${userID}`
+const checkUserQuery = userID => `SELECT id FROM users WHERE id=${userID}`
+const listQuery = `SELECT * FROM users WHERE admin=true`
 
-  if (paramIndex < 0) {
-    paramIndex = process.argv.indexOf('-u')
-  }
+async function getUserID () {
+  return new Promise((resolve, reject) => {
+    let paramIndex = process.argv.indexOf('--user-id')
 
-  const userID = process.argv[paramIndex + 1]
+    if (paramIndex < 0) {
+      paramIndex = process.argv.indexOf('-u')
+    }
 
-  if (paramIndex < 0 || !userID) {
-    console.log('\x1b[31m')
-    console.log('User id not found.')
-    console.log('\x1b[33m')
-    console.log('Pass the user id through --user-id or -u.')
-    console.log('Ex: "make --user-id 1" or "unmake --user-id 1"')
-    console.log('\x1b[0m')
-    process.exit()
-  }
+    const userID = process.argv[paramIndex + 1]
 
-  return userID
+    if (paramIndex < 0 || !userID) {
+      console.log('\x1b[31m')
+      console.log('User id not found.')
+      console.log('\x1b[33m')
+      console.log('Pass the user id through --user-id or -u.')
+      console.log('Ex: "make --user-id 1" or "unmake --user-id 1"')
+      console.log('\x1b[0m')
+      process.exit()
+    }
+
+    db.all(checkUserQuery(userID), (error, rows) => {
+      if (rows.length) {
+        resolve(userID)
+      } else {
+        console.log('\x1b[31m')
+        console.log('User not found.')
+        console.log('\x1b[0m')
+        process.exit()
+      }
+    })
+  })
 }
 
 const commands = {
   make: process.argv.indexOf('make') > -1,
-  unmake: process.argv.indexOf('unmake') > -1
+  unmake: process.argv.indexOf('unmake') > -1,
+  list: process.argv.indexOf('list')
 }
 
 let hasCommand
@@ -44,14 +61,22 @@ if (!hasCommand) {
   process.exit()
 }
 
-const userID = getUserID()
-
-const query = admin => `UPDATE users SET admin=${admin} WHERE id=${userID}`
-
-db.serialize(() => {
+db.serialize(async () => {
   if (commands.make) {
-    db.run(query(true))
+    const userID = await getUserID()
+    db.run(makeQuery(true, userID))
   } else if (commands.unmake) {
-    db.run(query(false))
+    const userID = await getUserID()
+    db.run(makeQuery(false, userID))
+  } else if (commands.list) {
+    db.all(listQuery, (error, rows) => {
+      if (rows.length === 0) {
+        console.log('\x1b[33m')
+        console.log('Admins not found.')
+        console.log('\x1b[0m')
+      } else {
+        console.table(rows)
+      }
+    })
   }
 })
